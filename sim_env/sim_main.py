a17ab335fb1449dbbe08b1b6de02dd56263a3dc8
vchristoffersson <vchristoffersson@gmail.com>
2018-10-25 08:57:13 +0200

    added files

--- sim_env/sim_main.py
+++ sim_env/sim_main.py
@@ -10,40 +10,44 @@ action_size = 4
 agent = Agent(
             state_size = state_size, 
             action_size = action_size, 
-            discount = 0.8, 
-            eps = 0.999, 
-            eps_decay = 0.995, 
-            eps_min = 0.00001, 
+            discount = 0.9, 
+            eps = 1, 
+            eps_decay = 1, 
+            eps_min = 0.01, 
             l_rate = 0.001
             )
 
 #Train agent
-episodes = 10000
+episodes = 150000
 steps = 200
 goalCounter = 0
+stepCounter = 0
+avg = -1
+ep = 0
 
 for ep in range(episodes):
+    ep += 1
     state = env.reset()
 
     for step in range(steps):
         action = int(agent.action(state))
-
-        #print("action: {}".format(action))
-
-        next_state, reward, done = env.step(action)
         
-        #print("reward: {}, done: {}, step: {}, action: {} ".format(reward, done, step, action))
+        next_state, reward, done = env.step(action) 
 
-        agent.remember(state, action, reward, next_state, done)
+        #agent.remember(state, action, reward, next_state, done)
 
         state = next_state
 
         if done:
             if reward > 1: 
                 goalCounter += 1
+                stepCounter += step
+                avg = stepCounter / goalCounter
 
-            print("episode {} done, after {} steps, reward: {}, goal #{}".format(ep, step, reward, goalCounter))
             break
 
-    agent.replay(64)
-    agent.save_model()
+    if ep % 1000 == 0:    
+        print("episode {} done, goal found {} times with an avg steps of {}".format(ep, goalCounter, avg))
+    
+    #agent.replay(64)
+    #agent.save_model()
